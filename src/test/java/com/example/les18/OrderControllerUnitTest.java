71e477d8d4046d7b5b9862ab8ce50180cba31776
robertjan <robertjan@eliascc.nl>
2025-03-31 12:14:09 +0200

    Replaced deprecated @MockBean with @MockitoBean.

--- src/test/java/com/example/les18/OrderControllerUnitTest.java
+++ src/test/java/com/example/les18/OrderControllerUnitTest.java
@@ -9,8 +9,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -26,10 +26,10 @@ class OrderControllerUnitTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     JwtService jwtService;
 
-    @MockBean
+    @MockitoBean
     OrderService orderService;
 
     @Test
